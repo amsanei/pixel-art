1244013d2dbb32709239a6b6b9e591feebc12611
Amir mohammad <am.sanei79@gmail.com>
2024-05-01 13:30:05 +0330

    Add functionalty

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import happyIcon from "./assets/happy.png";
 import chatIcon from "./assets/chat.png";
+import { useState } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(1);
+    const [saidYes, setSaidYes] = useState(false);
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="w-1/3 bg-white bg-opacity-70 shadow px-4 py-6 rounded-xl">
                 <div className="flex items-center gap-4 justify-center mb-8">
-                    <img src={happyIcon} alt="happy" />
-                    <img src={chatIcon} alt="chat" />
+                    {saidYes ? (
+                        <img src={happyIcon} alt="happy" />
+                    ) : (
+                        <img
+                            src={chatIcon}
+                            alt="chat"
+                            className="animate-pulse"
+                        />
+                    )}
                 </div>
                 <div className="text-xl text-center">
-                    Hello! Can you awnser this hard qustion?
+                  {saidYes ? "Tank you darling, i love my self too XXX" : "Hello! Can you awnser this hard qustion?"}
+                    
                 </div>
-                <div className="text-3xl text-center">Do You love me? *_*</div>
-                <div className="flex justify-center gap-2 mt-8">
-                    <button className="bg-white rounded-full opacity-30 px-8 py-1">
-                        no
-                    </button>
-                    <button className="bg-pink-400 uppercase font-bold rounded-full px-8 py-1">
-                        yes
-                    </button>
-                </div>
+                {!saidYes && (
+                    <>
+                        <div className="text-3xl text-center">
+                            Do You love me? *_*
+                        </div>
+                        <div className="flex justify-center items-center gap-2 mt-8">
+                            <button
+                                style={{ fontSize: 22 - counter + "px" }}
+                                className="bg-white rounded-full px-8 py-1"
+                                onClick={() => setCounter((prev) => prev + 2)}
+                            >
+                                no
+                            </button>
+                            <button
+                                style={{ fontSize: counter + 22 + "px" }}
+                                className="bg-pink-400 uppercase font-bold rounded-full px-8 py-1"
+                                onClick={() => setSaidYes(true)}
+                            >
+                                yes
+                            </button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
