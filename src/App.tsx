bb3e9a4f452e031babc3695402ab3b86e7872959
Amir mohammad <am.sanei79@gmail.com>
2024-05-01 13:20:38 +0330

    Create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-function App() {
+import happyIcon from "./assets/happy.png";
+import chatIcon from "./assets/chat.png";
 
-  return (
-    <div>
-      hello
-    </div>
-  )
+function App() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="w-1/3 bg-white bg-opacity-70 shadow px-4 py-6 rounded-xl">
+                <div className="flex items-center gap-4 justify-center mb-8">
+                    <img src={happyIcon} alt="happy" />
+                    <img src={chatIcon} alt="chat" />
+                </div>
+                <div className="text-xl text-center">
+                    Hello! Can you awnser this hard qustion?
+                </div>
+                <div className="text-3xl text-center">Do You love me? *_*</div>
+                <div className="flex justify-center gap-2 mt-8">
+                    <button className="bg-white rounded-full opacity-30 px-8 py-1">
+                        no
+                    </button>
+                    <button className="bg-pink-400 uppercase font-bold rounded-full px-8 py-1">
+                        yes
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
